0637667430b25b814c0865c88add6b6114b07a58
Abhay kumar <mk9416822@gmail.com>
2025-03-16 00:23:44 +0530

    file structure done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from '.pages/Home.jsx'
+import Verify_Account from '.pages/Verify_Account.jsx'
+import Reset_Password from '.pages/Reset_Password.jsx'
+import Login from '.pages/Login.jsx'
 
 const App = () => {
   return (
-    <div className='text-lime-400 text-4xl'>App</div>
+    <Router>  {/* ✅ Wrap everything inside <BrowserRouter> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/verify_account" element={<Verify_Account />} />
+        <Route path="/reset_password" element={<Reset_Password />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
