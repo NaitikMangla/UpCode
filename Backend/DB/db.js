26af153931057174ef096fa82a01f89c16947d7f
Abhay kumar <mk9416822@gmail.com>
2025-03-17 20:02:54 +0530

    done

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/UpCode-Auth`);
-
         console.log('MongoDB connected');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
