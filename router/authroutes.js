4c05f32f048f25b6caae6b9949f6d340cfe0eb1e
Abhay kumar <mk9416822@gmail.com>
2025-03-18 13:29:17 +0530

    Done Auth

---
+++ router/authroutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const { register, login, logout, sendverifyOTP, verifyemail, isAuthenticated, sendResetOTP, resetPassword } = require('../controller/authcontroller');
+const userAuth = require('../middleware/userauth');
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/send_verify_otp', userAuth, sendverifyOTP);
+router.post('/verify_account', userAuth, verifyemail);
+router.post('/is_authenticated', userAuth, isAuthenticated);
+router.post('/send_reset_otp', sendResetOTP);
+
+router.post('/reset_password', resetPassword); // This route requires userAuth middleware, which verifies the token in the request header
+
+module.exports = router;
